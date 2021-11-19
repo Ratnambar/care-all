7caedf19ea7e43a397fb254769a5c619a598022e
Ratnambar <ratnambar123gupta@gmail.com>
2021-11-19 12:58:35 +0530

    changing some variable

--- care/settings.py
+++ care/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-# import django_heroku
+import django_heroku
 from logging import DEBUG
 from pathlib import Path
 import os
@@ -23,15 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = 'django-insecure-$fxd)^fecw3&frmjdvu84h^8+)n20c2!tgmy9(#&*1@nn0+p=z'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
-# ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -150,5 +149,12 @@ SIMPLE_JWT = {
 }
 
 
+EMAIL_BACKEND = ''
+EMAIL_HOST_USER = 'guptaratnambar@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = ''
 
-# django_heroku.settings(locals())
+
+django_heroku.settings(locals())
