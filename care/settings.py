d865cbc799b472a16ea588b1e52d2300f465be6c
Ratnambar <ratnambar123gupta@gmail.com>
2021-11-19 07:33:37 +0530

    changed settings

--- care/settings.py
+++ care/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-# import django_heroku
+import django_heroku
 from logging import DEBUG
 from pathlib import Path
 import os
@@ -23,15 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = 'django-insecure-$fxd)^fecw3&frmjdvu84h^8+)n20c2!tgmy9(#&*1@nn0+p=z'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
-# ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -45,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'accounts',
-    'rest_framework_simplejwt',
     'rest_framework.authtoken',
 ]
 
@@ -151,4 +149,4 @@ SIMPLE_JWT = {
 }
 
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
